4b9eaf0c8d91bd5960275de018190f595fbfc6b3
Krystyna Kruchkovska <kruchok883@gmail.com>
2019-01-24 21:34:51 +0200

    Added graph

--- HistoryOfWeatherTrends/View/GraphVC.swift
+++ HistoryOfWeatherTrends/View/GraphVC.swift
@@ -14,19 +14,37 @@ class GraphVC: UIViewController,ScrollableGraphViewDataSource {
     var weatherViewModel:WeatherViewModel!
     var selectedElement:String!
     var weatherValues:[Double]!
-    var graphView:ScrollableGraphView!
     
     let linePlot = LinePlot(identifier: "darkLine")
     let dotPlot = DotPlot(identifier: "darkLineDot")
     let referenceLines = ReferenceLines()
     
+    
+    @IBOutlet weak var graphView: ScrollableGraphView!
+    
 
     override func viewDidLoad() {
-        self.graphView = ScrollableGraphView(frame: self.view.frame, dataSource: self)
+        self.graphView.dataSource = self
         self.setUpValues()
         self.setUpGraphView()
         self.view.addSubview(graphView)
     }
+    
+    func createRangeOfNumbersforY() -> [Double]{
+        var minValue = Int(weatherValues.min()!)
+        let maxValue = Int(weatherValues.max()!)
+        
+        var rangeOfNumbersforY:[Double] = []
+        
+        while minValue <= maxValue {
+            rangeOfNumbersforY.append(Double(minValue))
+            minValue += 2
+        }
+        
+        return rangeOfNumbersforY
+    }
+    
+    
     
     func setUpValues(){
         let weatherDataArray = weatherViewModel.weatherDataPoints
@@ -46,13 +64,13 @@ class GraphVC: UIViewController,ScrollableGraphViewDataSource {
     
     func setUpLinePlot() {
         self.linePlot.lineWidth = 1
-        self.linePlot.lineColor = UIColor(red: 119, green: 119, blue: 119, alpha: 1)
+        self.linePlot.lineColor = UIColor(red: 119/255, green: 119/255, blue: 119/255, alpha: 1)
         self.linePlot.lineStyle = ScrollableGraphViewLineStyle.smooth
         self.linePlot.shouldFill = true
         self.linePlot.fillType = ScrollableGraphViewFillType.gradient
         self.linePlot.fillGradientType = ScrollableGraphViewGradientType.linear
-        self.linePlot.fillGradientStartColor = UIColor(red: 85, green: 85, blue: 85, alpha: 1)
-        self.linePlot.fillGradientEndColor = UIColor(red: 68, green: 68, blue: 68, alpha: 1)
+        self.linePlot.fillGradientStartColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
+        self.linePlot.fillGradientEndColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 1)
         
         self.linePlot.adaptAnimationType = ScrollableGraphViewAnimationType.elastic
     }
@@ -65,24 +83,21 @@ class GraphVC: UIViewController,ScrollableGraphViewDataSource {
         
         self.referenceLines.positionType = .absolute
         // Reference lines will be shown at these values on the y-axis.
-        self.referenceLines.absolutePositions = [10, 20, 25, 30]
-        self.referenceLines.includeMinMax = false
+        self.referenceLines.absolutePositions = self.createRangeOfNumbersforY()
         
         self.referenceLines.dataPointLabelColor = UIColor.white.withAlphaComponent(0.5)
     }
-    
     
     func setUpGraphView() {
         self.setUpLinePlot()
         self.setupDotPlot()
         self.setUpReferace()
         
-        self.graphView.backgroundFillColor = UIColor(red: 51, green: 51, blue: 51, alpha: 1)
+        self.graphView.backgroundFillColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
         self.graphView.dataPointSpacing = 80
         
         self.graphView.shouldAnimateOnStartup = true
         self.graphView.shouldAdaptRange = true
-        self.graphView.shouldRangeAlwaysStartAtZero = true
         
         self.graphView.addPlot(plot: self.dotPlot)
         self.graphView.addPlot(plot: self.linePlot)
@@ -90,14 +105,7 @@ class GraphVC: UIViewController,ScrollableGraphViewDataSource {
     }
     
     func value(forPlot plot: Plot, atIndex pointIndex: Int) -> Double {
-        switch(plot.identifier) {
-        case "darkLine":
-            return weatherValues[pointIndex]
-        case "darkLineDot":
-            return weatherValues[pointIndex]
-        default:
-            return 0
-        }
+        return self.weatherValues[pointIndex]
     }
     
     func label(atIndex pointIndex: Int) -> String {
@@ -105,40 +113,10 @@ class GraphVC: UIViewController,ScrollableGraphViewDataSource {
     }
     
     func numberOfPoints() -> Int {
-         return weatherValues.count
+         return self.weatherValues.count
     }
     
 
-    
-    
-    
-//    func setupFirstChart() {
-//
-//        graphView.setScaleEnabled(true)
-//        graphView.pinchZoomEnabled = true
-//
-//        let weatherDataArray = weatherViewModel.weatherDataPoints
-//
-//        let weatherValues = weatherDataArray.map {(element) -> Double? in
-//            return self.weatherViewModel.getPointValue(weatherDataPoint: element, selectedElement: self.selectedElement)
-//        }
-//
-//        var unwrapedWeatherValues = weatherValues.compactMap{ $0 }
-//
-//
-//        let values = (0..<unwrapedWeatherValues.count).map { (i) -> ChartDataEntry in
-//            let val = unwrapedWeatherValues[i]
-//            return ChartDataEntry(x: Double(i), y: val)
-//        }
-//
-//
-//        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
-//        set1.mode = .cubicBezier
-//             //my phone is dead one sec ok
-//        self.graphView.data = LineChartData(dataSet: set1)
-//    }
-    
-    
     
 }
 
